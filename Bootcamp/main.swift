aec0c25b950ac0f114cf4d372a0894fc7fe2cd72
Hashfi Alfian Ciyuda <hashfi.ciyuda@bukalapak.com>
2019-09-10 13:08:45 +0700

    Initial Commit

---
+++ Bootcamp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Bootcamp
+//
+//  Created by Hashfi Alfian on 10/09/19.
+//  Copyright © 2019 Bukalapak. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
